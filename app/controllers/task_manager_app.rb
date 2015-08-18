d24227738036e7fcb0da8a000c6d12a098c9c45e
selfup <boudinot.regis@yahoo.com>
2015-08-18 14:01:03 -0600

    movin along

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -1,3 +1,5 @@
+require 'models/task_manager'
+
 class TaskManagerApp < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__), '..')
 
@@ -8,5 +10,14 @@ class TaskManagerApp < Sinatra::Base
 	get '/tasks' do
 		@tasks = ["task1", "task2", "task3"]
 		erb :index
+	end
+
+	get '/tasks/new' do
+		erb :new
+	end
+
+	post '/tasks' do
+		TaskManager.create(params[:task])
+    redirect '/tasks'
 	end
 end
